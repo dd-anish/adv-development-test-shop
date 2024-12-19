e95321ee91267ed2b1f36b0bfea709174c906a49
dd-anish <anish@deardigital.com>
2024-12-19 11:24:16 +0530

    Product Weight Dynamic Specs working | Removed Old script Codes

--- assets/product-info.js
+++ assets/product-info.js
@@ -191,7 +191,8 @@ if (!customElements.get('product-info')) {
           updateSourceFromDestination('Inventory', ({ innerText }) => innerText === '');
           updateSourceFromDestination('Volume');
           updateSourceFromDestination('Price-Per-Item', ({ classList }) => classList.contains('hidden'));
-
+          updateSourceFromDestination("Specifications");
+          
           this.updateQuantityRules(this.sectionId, html);
           this.querySelector(`#Quantity-Rules-${this.dataset.section}`)?.classList.remove('hidden');
           this.querySelector(`#Volume-Note-${this.dataset.section}`)?.classList.remove('hidden');
